aaefe4298a6898b0eb01517a5c7ca85d9452e0fb
mundusjp <raymon.prijati@gmail.com>
2018-07-19 17:04:04 +0700

    Gue mau ngepush ya
    sip

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,11 +1,17 @@
 <?php
   class Pages extends CI_Controller{
-    public function view($page ='home'){
+    public function view($page ='index'){
       if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
         show_404();
       }
       $data['title'] = ucfirst($page);
 
-      $this->load->view('pages/'.$page,$data);
+      $this->load->view('templates/dashboard/header');
+      $this->load->view('templates/dashboard/rightbar');
+      $this->load->view('templates/dashboard/topbar');
+      $this->load->view('templates/dashboard/leftbar');
+      $this->load->view('templates/dashboard/infobox');
+        $this->load->view('pages/'.$page,$data);
+      $this->load->view('templates/dashboard/footer');
     }
   }
