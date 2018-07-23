1de2b73afba5799fd1f15824a5bc343802f54c50
mundusjp <raymon.prijati@gmail.com>
2018-07-23 10:39:17 +0700

    untuk fargit

--- application/views/pages/PantauDivisi.php
+++ application/views/pages/PantauDivisi.php
@@ -14,109 +14,70 @@
                 <h4 class="text-themecolor">Penggunaan Divisi</h4>
             </div>
         </div>
+
         <!-- ============================================================== -->
-        <!-- End Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- ABAIKAN!! HANYA DEMO -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Campaign -->
+        <!-- Ini working space teman teman. Tinggal tambah table dkk-->
         <!-- ============================================================== -->
         <div class="row">
-            <div class="col-lg-6">
-                <div class="row">
-                    <!-- column -->
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">EXPIRED INVENTORIES</h5>
-                                <div class="d-flex m-t-30 m-b-20 no-block align-items-center">
-                                    <span class="display-5 text-danger"><i class="icon-close"></i></span>
-                                    <a href="javscript:void(0)" class="link display-5 ml-auto">23</a>
-                                </div>
-                            </div>
-                        </div>
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title"> Pilih Divisi </h4>
+                <h6 class="card-subtitle"> Tabel akan muncul setelah anda memilih divisi </h6>
+                  <div class="row">
+                    <div class="col-6">
+                      <select class="select2 form-control custom-select col-6" style="width: 40%; height:36px;">
+                        <option>Pilihan</option>
+                        <optgroup label="Terminal Operasi 1">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                        <optgroup label="Terminal Operasi 2">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                        <optgroup label="Terminal Operasi 3">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                      </select>
                     </div>
-                    <!-- column -->
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">NEED RESTOCK</h5>
-                                <div class="d-flex m-t-30 m-b-20 no-block align-items-center">
-                                    <span class="display-5 text-purple"><i class="icon-basket-loaded"></i></span>
-                                    <a href="javscript:void(0)" class="link display-5 ml-auto">169</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- column -->
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">OPEN PROJECTS</h5>
-                                <div class="d-flex m-t-30 m-b-20 no-block align-items-center">
-                                    <span class="display-5 text-primary"><i class="icon-folder-alt"></i></span>
-                                    <a href="javscript:void(0)" class="link display-5 ml-auto">311</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- column -->
-                    <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">NEW INVOICES</h5>
-                                <div class="d-flex m-t-30 m-b-20 no-block align-items-center">
-                                    <span class="display-5 text-success"><i class="icon-wallet"></i></span>
-                                    <a href="javscript:void(0)" class="link display-5 ml-auto">117</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- column -->
-                </div>
+                    <div class="col-6">
+                      <select class="select2 form-control custom-select col-6" style="width: 40%; height:36px;">
+                        <option>Pilihan</option>
+                        <optgroup label="Terminal Operasi 1">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                        <optgroup label="Terminal Operasi 2">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                        <optgroup label="Terminal Operasi 3">
+                          <option value="TK">Teknik</option>
+                          <option value="HR">SDM</option>
+                          <option value="OP">Operasional</option>
+                          <option value="RD">R&D</option>
+                        </optgroup>
+                      </select>
+                  </div>
+              </div>
             </div>
-            <div class="col-lg-6">
-                <div class="news-slide m-b-15">
-                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                        <!-- Carousel items -->
-                        <div class="carousel-inner">
-                            <div class="active carousel-item">
-                                <div class="overlaybg"><img src="<?php echo base_url('/assets/vertical/images/news/news1.jpg')?>" class="img-fluid" /></div>
-                                <div class="news-content carousel-caption"><span class="label label-danger label-rounded">Primary</span>
-                                    <h4>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</h4> <a href="javascript:void(0)" class="btn btn-rounded btn-secondary">Read More</a></div>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="overlaybg"><img src="<?php echo base_url('/assets/vertical/images/news/news1.jpg')?>" /></div>
-                                <div class="news-content carousel-caption"><span class="label label-primary label-rounded">Primary</span>
-                                    <h4>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</h4> <a href="javascript:void(0)" class="btn btn-rounded btn-secondary">Read More</a></div>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="overlaybg"><img src="<?php echo base_url('/assets/vertical/images/news/news1.jpg')?>" /></div>
-                                <div class="news-content carousel-caption"><span class="label label-success label-rounded">Primary</span>
-                                    <h4>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</h4> <a href="javascript:void(0)"class="btn btn-rounded btn-secondary">Read More</a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+
+
+        <!-- ============================================================== --?>
+        <!-- Ini adalah akhir dari working space. No more coding below      -->
         <!-- ============================================================== -->
-        <!-- End Campaign -->
-        <!-- ============================================================== -->
-                <!-- ============================================================== -->
-                <!-- DEMONYA SELESAI! Working space di bawah!-->
-                <!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Ini working space teman teman. Tinggal tambah table dkk-->
-<!-- ============================================================== -->
-
-
-
-
-
-
-<!-- ============================================================== -->
-<!-- Ini adalah akhir dari working space. No more coding below      -->
-<!-- ============================================================== -->
