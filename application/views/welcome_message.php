a3e5ca45c148c3b6e8ea654d1594ee3d670a9813
mundusjp <raymon.prijati@gmail.com>
2018-08-29 16:56:46 +0700

    optimize recover password

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -82,16 +82,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <div class="form-group ">
                             <div class="col-xs-12">
                                 <h3>Recover Password</h3>
-                                <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+                                <p class="text-muted">Please Contact your <span class="text-primary">Administrator!</span> </p>
                             </div>
-                        </div>
-                        <div class="form-group ">
-                            <div class="col-xs-12">
-                                <input class="form-control" type="text" required="" placeholder="Email"> </div>
                         </div>
                         <div class="form-group text-center m-t-20">
                             <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block waves-effect waves-light" type="submit">Reset</button>
+                                <button class="btn btn-primary btn-lg btn-block waves-effect waves-light">Back</button>
                             </div>
                         </div>
                     </form>
